22b4af416d8298755caabf94e6786cf1de422ce0
Evgeniy_Zhelak <Ev-ZHelak@mail.ru>
2025-03-28 12:19:08 +0700

    refactor code

--- main.py
+++ main.py
@@ -5,14 +5,29 @@ from sys import exit
 BASE_FILE = 'data.pkl'
 
 
-def show_menu():
-    "Показать приветствие и меню"
+def welcome():
+    "Приветствие"
     print('~ You are welcomed by a list of tasks ~\n')
-    print('1. Add the task')
-    print('2. A complete list of tasks')
-    print('3. Delete the task')
-    print('4. Clean the list of tasks')
-    print('q. Exit')
+
+
+def show_menu(show_flag: bool = True) -> dict:
+    "Показать меню"
+    menu = {
+        "1": "Add the task",
+        "2": "A complete list of tasks",
+        "3": "Delete the task",
+        "4": "Clean the list of tasks",
+        "q": "Exit"
+    }
+    if show_flag:
+        for k, v in menu.items():
+            print(f"{k}. {v}")
+    return menu
+
+
+def stop_waiting_user():
+    "Остановка программы ожидание действий пользователя"
+    input("continue Enter...")
 
 
 def cleaning_screen():
@@ -38,7 +53,7 @@ def get_base() -> dict:
 
 
 def save_base(data: dict):
-    "Сахранить в файл базы данных"
+    "Сахранить данные в файл базы данных"
     with open(BASE_FILE, 'wb') as file:
         pickle.dump(data, file)
 
@@ -46,9 +61,9 @@ def save_base(data: dict):
 def user_input() -> str:
     "Ввод пользователя и проверка корректности"
     while True:
-        s = input("\nYour Select: ")
-        if not (len(s) == 1 and s in ('1', '2', '3', '4', 'q', 'Q')):
-            print(f"invalid input not {('1', '2', '3', '4', 'q')}")
+        s = input("\nYour Select: ").lower()
+        if not (len(s) == 1 and s in show_menu(show_flag=False).keys()):
+            print(f"invalid input not ({', '.join(show_menu(show_flag=False).keys())})")
         else:
             return s
 
@@ -87,17 +102,16 @@ def delete_task():
     "Удаление задачи"
     data_base = get_base()
     list_tasks = data_base['List_Tasks']
-    while True:
-        index_task = int(input("Indicate the number of the deleted task: "))
-        if not list_tasks:
-            print("Empty\n")
-            break
-        if 1 <= index_task <= len(list_tasks):
-            res = list_tasks.pop(index_task - 1)
-            print(f"Delete task - {res}\n")
-            break
-        else:
-            print("There is no such task\n")
+    index_task = int(input("Indicate the number of the deleted task: "))
+    if not list_tasks:
+        print("Empty\n")
+
+    if 1 <= index_task <= len(list_tasks):
+        res = list_tasks.pop(index_task - 1)
+        print(f"Delete task - {res}\n")
+
+    else:
+        print("There is no such task\n")
     save_base(data_base)
 
 
@@ -107,7 +121,7 @@ if __name__ == '__main__':
     create_base()
 
     while True:
-        # cleaning_screen()
+        welcome()
         show_menu()
         result_input = user_input()
         match result_input:
@@ -119,5 +133,7 @@ if __name__ == '__main__':
                 delete_task()
             case "4":
                 clean_list_tasks()
-            case "q" | "Q":
+            case "q":
                 exit()
+        stop_waiting_user()
+        cleaning_screen()
